be0c9e41ee6b4c95f3560b8e0b4bf7643b0d45d1
Timothy McCarthy <ebh042@gmail.com>
2019-03-10 09:15:05 +1100

    Add Postcode

---
+++ core/src/main/scala/au/id/tmm/ausgeo/Postcode.scala
@@ -0,0 +1,37 @@
+package au.id.tmm.ausgeo
+
+import scala.runtime.ScalaRunTime
+import scala.util.matching.Regex
+
+final case class Postcode private (asString: String) extends AnyVal {
+  def copy(asString: String = this.asString): Either[Postcode.CreationError, Postcode] = Postcode(asString)
+}
+
+object Postcode {
+
+  private val validPostcodePattern: Regex = """^\d{4}$""".r
+  private val onlyNumbersPattern: Regex = """^\d+$""".r
+
+  def apply(asString: String): Either[CreationError, Postcode] = asString match {
+    case validPostcodePattern() => Right(new Postcode(asString))
+    case onlyNumbersPattern() => Left(CreationError.InvalidLength(asString.length))
+    case nonDigitsString => Left(CreationError.NotNumeric(nonDigitsString))
+  }
+
+  def makeUnsafe(asString: String): Postcode = Postcode(asString) match {
+    case Right(postcode) => postcode
+    case Left(e) => throw e
+  }
+
+  sealed abstract class CreationError extends Exception with Product {
+    override def getMessage: String = ScalaRunTime._toString(this)
+  }
+
+  object CreationError {
+    final case class InvalidLength(actualLength: Int) extends CreationError
+    final case class NotNumeric(badPostcode: String) extends CreationError
+  }
+
+  implicit val ordering: Ordering[Postcode] = Ordering.by(_.asString)
+
+}
---
+++ core/src/test/scala/au/id/tmm/ausgeo/PostcodeSpec.scala
@@ -0,0 +1,43 @@
+package au.id.tmm.ausgeo
+
+import au.id.tmm.utilities.testing.ImprovedFlatSpec
+
+class PostcodeSpec extends ImprovedFlatSpec {
+
+  "a postcode" can "not be made if it is less than 4 digits" in {
+    assert(Postcode("123") === Left(Postcode.CreationError.InvalidLength(3)))
+  }
+
+  it can "not be made if it is more than 4 digits" in {
+    assert(Postcode("12345") === Left(Postcode.CreationError.InvalidLength(5)))
+  }
+
+  it can "not be made if it contains alphabetic characters" in {
+    assert(Postcode("a234") === Left(Postcode.CreationError.NotNumeric("a234")))
+  }
+
+  it can "not be made if it contains Arabic numerals" in {
+    assert(Postcode("٣٠٠٠") === Left(Postcode.CreationError.NotNumeric("٣٠٠٠")))
+  }
+
+  it can "not be copied to be invalid" in {
+    assert(Postcode.makeUnsafe("3000").copy("invalid") === Left(Postcode.CreationError.NotNumeric("invalid")))
+  }
+
+  it should "be ordered according to the code" in {
+    val unordered = List(
+      Postcode.makeUnsafe("5000"),
+      Postcode.makeUnsafe("0500"),
+      Postcode.makeUnsafe("3000"),
+    )
+
+    val expectedSorted = List(
+      Postcode.makeUnsafe("0500"),
+      Postcode.makeUnsafe("3000"),
+      Postcode.makeUnsafe("5000"),
+    )
+
+    assert(unordered.sorted === expectedSorted)
+  }
+
+}
